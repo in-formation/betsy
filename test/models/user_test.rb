c55c2a546a5bd60596df4d87888248e8af252395
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-23 15:39:47 -0700

    Added user model validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    
+    it "can be valid" do
+      is_valid = users(:valid_user).valid?
+
+      assert( is_valid )
+    end
+
+    it "is invalid if there is no name" do
+      invalid_user = users(:valid_user)
+      invalid_user.name = ""
+
+      is_valid = invalid_user.valid?
+
+      refute( is_valid )
+    end
+
+    it "gives an error message if the name given is not unique" do
+      invalid_user = users(:valid_user2)
+      invalid_user.name = "ValidName"
+
+      is_valid = invalid_user.valid?
+
+      refute( is_valid )
+    end
+
+    it "is invalid if there is no email" do
+      invalid_user = users(:valid_user)
+      invalid_user.email = ""
+
+      is_valid = invalid_user.valid?
+
+      refute( is_valid )
+    end
+
+    it "gives an error message if the email given is not unique" do
+      invalid_user = users(:valid_user2)
+      invalid_user.email = "valid@user.org"
+
+      is_valid = invalid_user.valid?
+
+      refute( is_valid )
+    end
+  end
 end
