750feb2b5e48ee6cb29884128961880460ecec69
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-23 11:14:07 -0700

    added routes for user and product

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: => 'homepages#index'
+  
+  resources :users, only:[:show, :index, :destroy]
+
+  resources :products, except: :destroy
 end
