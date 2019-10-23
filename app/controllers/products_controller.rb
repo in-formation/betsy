12a6f3fd0ac4fc1387d291f39bd068d0c5f1a68e
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-23 14:15:55 -0700

    created products controller actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,59 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+  
+  def show
+    product_id = params[:id].to_i
+    @product = Product.find_by(id: product_id)
+    if @product.nil?
+      flash[:error] = "Product not found"
+      redirect_to products_path
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "#{@product.name} successfully saved!"
+      redirect_to product_path(@product.id)
+    else
+      flash[:error] = "Product not successfully saved"
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      flash[:error] = "That product does not exist"
+      redirect_to products_path
+      return
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      flash[:error] = "That product does not exist"
+      redirect_to products_path
+      return
+    elsif @product.update(product_params)
+      flash[:success] = "#{@product.name} successfully updated!"
+      redirect_to product_path(@product.id)
+      return
+    else
+      flash.now[:error] = "#{@product.name} not successfully updated!"
+      render :edit
+    end
+  end
+
+  private
+  def product_params
+    return params.require(:product).permit(:name, :qty, :price, :description, :status)
+  end
 end
