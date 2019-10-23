df636ff079c1957b1779fa54a03d017ab0b6178f
Brianna Kemp <kemp.bri@gmail.com>
2019-10-23 15:38:14 -0700

    merged master to products branch to create user, take 2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,7 @@ require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/autorun'
 require 'minitest/reporters'
-<<<<<<< HEAD
 require 'pry'
-=======
->>>>>>> master
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -15,34 +12,30 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   
-<<<<<<< HEAD
-  # Add more helper methods to be used by all tests here...
-=======
   def setup
     OmniAuth.config.test_mode = true
   end
-
+  
   def mock_auth_hash(user)
     return {
-      provider: user.provider,
-      uid: user.uid,
-      info: {
-        email: user.email,
-        nickname: user.name
-        # You can add more fields from
-        # The omniauth hash here
-      }
-    }
-  end
+    provider: user.provider,
+    uid: user.uid,
+    info: {
+    email: user.email,
+    nickname: user.name
+    # You can add more fields from
+    # The omniauth hash here
+  }
+}
+end
 
-  def perform_login(user = User.first)
-    
-    OmniAuth.config.mock_auth[:github] = 
-        OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-    get auth_callback_path(:github)
-
-    return user
-  end
->>>>>>> master
+def perform_login(user = User.first)
+  
+  OmniAuth.config.mock_auth[:github] = 
+  OmniAuth::AuthHash.new(mock_auth_hash(user))
+  
+  get auth_callback_path(:github)
+  
+  return user
+end
 end
