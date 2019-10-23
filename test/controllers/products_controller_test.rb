79314689d0737cf4239f40af3d663352314804c4
Brianna Kemp <kemp.bri@gmail.com>
2019-10-23 15:30:05 -0700

    finished product index and new tests, create in progress

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do 
+    it "succeeds when there are products" do
+      get products_path
+      
+      must_respond_with :success
+    end
+    
+  end
+  
+  
+  describe "new" do 
+    it "succeeds" do
+      get new_product_path
+      
+      must_respond_with :success
+    end
+    
+  end
+  
+  describe "create" do
+    it "creates a new product with valid data" do
+      new_product = { product:  {name: "new product", qty: 7, price: 149.99, description: "This product is new", status: "active"} }
+      # binding.pry
+      expect{
+      post products_path, params: new_product
+    }.must_change "Product.count", 1
+    
+    new_product_id = Product.find_by(name: "new product").id
+    
+    must_respond_with :redirect
+    must_redirect_to product_path(new_product_id)
+    
+  end
+  
+  it "renders bad_request and does not update the DB" do
+    bad_product = { product:  {name: "", qty: 7, price: 149.99, description: "This product is new", status: "active"} }
+    
+    expect {
+    post products_path, params: bad_product
+  }.wont_change "Product.count"
+  
+  assert_template :new
+  
+end
+
+end
 end
