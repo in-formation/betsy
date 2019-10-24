e189248e4e5cdd0cab15e631698485f38ffd1c6d
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-24 14:58:25 -0700

    added another test to review controller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -16,25 +16,31 @@ describe ReviewsController do
     
     it "creates a new review with valid data" do      
       
-
       new_review = { review:  {rating: 4, review: "this sucked", product_id: product.id } }
       
       expect{ post product_reviews_path(product.id), params: new_review }.must_differ 'Review.count', 1
-      
-      
       
       must_respond_with :redirect
       must_redirect_to product_path(product.id)
       
     end
     
+    it "will not create a review without a rating" do
+      new_review = { review:  {rating: nil, review: "this sucked", product_id: product.id } }
+      
+      expect{ post product_reviews_path(product.id), params: new_review }.wont_change 'Review.count'
+      
+      must_respond_with :bad_request
+      
+    end 
+    
     # it "renders bad_request and does not update the DB" do
     #   bad_product = { product:  {name: "", qty: 7, price: 149.99, description: "This product is new", status: "active"} }
-      
+    
     #   expect { post products_path, params: bad_product }.wont_change "Product.count"
-      
+    
     #   must_respond_with :redirect
-      
+    
     # end
     
   end
