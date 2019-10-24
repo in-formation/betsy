a29c767b3df72800634f3ccd38262b0821aad3b4
Brianna Kemp <kemp.bri@gmail.com>
2019-10-23 22:54:09 -0700

    Added validation tests and relationship test for user, relationship tests for categories, reviews, and order items still pending

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,80 @@
 require "test_helper"
 
 describe Product do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "can be instantiated" do
+      user = User.first
+      
+      product = Product.create(name: "product", price: 599.99, user_id: user.id)
+      
+      expect(product.valid?).must_equal true
+    end
+    
+    it "does not create a product if name is not present" do
+      user = User.first
+      
+      invalid_product = Product.create(name: nil, price: 599.99, user_id: user.id)
+      expect(invalid_product.valid?).must_equal false
+    end
+    
+    it "does not create a product if name is not unique" do
+      user = User.first
+      
+      product_1 = Product.create(name: "this is a product", price: 599.99, user_id: user.id)
+      product_2 = Product.create(name: "this is a product", price: 599.99, user_id: user.id)
+      
+      expect(product_2.valid?).must_equal false
+    end
+    
+    it "does not create a product if price is not present" do
+      user = User.first
+      
+      invalid_product = Product.create(name: "this is a product", price: nil, user_id: user.id)
+      expect(invalid_product.valid?).must_equal false
+    end
+    
+    it "does not create a product if price is not a float greater than 0" do
+      user = User.first
+      
+      invalid_product = Product.create(name: nil, price: -58.54, user_id: user.id)
+      expect(invalid_product.valid?).must_equal false
+    end
+    
+    it "does not create a product if price is not a float" do
+      user = User.first
+      
+      invalid_product = Product.create(name: nil, price: 58, user_id: user.id)
+      expect(invalid_product.valid?).must_equal false
+    end    
+  end
+  
+  describe "relationships" do
+    it "has a user" do
+      user = User.first
+      
+      product = Product.create(name: "product", price: 599.99, user_id: user.id)
+      
+      expect(product).must_respond_to :user
+      expect(product.user).must_be_kind_of User
+    end
+    
+    # it "has a list of voting users" do
+    #   product = Product.first
+    #   product.votes.each do |vote|
+    #     expect(vote.user_id).must_be_kind_of Integer
+    #   end
+    # end
+    
+    it "contains many categories" do
+      
+    end
+    
+    it "contains many order items" do
+      
+    end
+    
+    it "contains many reviews" do
+      
+    end
+  end
 end
