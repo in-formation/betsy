0dac0644133749732a00f2acf6f2d90fd848115a
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-28 15:22:08 -0700

    added order controller actions and order show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     if session[:order_id]
       @current_order = Order.find_by(id: session[:order_id])
     else
-      @current_order = Order.create
+      @current_order = Order.create(status: "pending")
       session[:order_id] = @current_order.id
     end
   end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,7 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find_by(id: params[:product_id])
     @review = Review.new(review_params)
+    @review.product_id = @product.id
     if @review.save
       flash[:status] = :success
       flash[:result_text] = "Successfully created review"
