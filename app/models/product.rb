eaeb47f7c63a26fb15f263d56aafde75b4b3b8ac
Brianna Kemp <kemp.bri@gmail.com>
2019-10-28 14:09:20 -0700

    spotlight added

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,35 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {only_float: true, greater_than: 0}
   validates :qty, presence: true, numericality: {only_integer: true} 
+  
+  def self.spotlight
+    products = Product.all.sort_by {|product| - product.created_at }
+    if products
+      return products[0..2]
+    else
+      return ["No products found"]
+    end
+  end
+  
+  def self.top5
+    products = Product.where(product.reviews: != nil).sort_by {|product| - product.avg_rating }
+    if products
+      return products[0..4]
+    else
+      return ["No products found"]
+    end
+    
+  end
+  
+  def avg_rating
+    reviews = Review.where(product_id: self.id)
+    ratings = reviews.map do |review|
+      review.rating
+    end
+    if ratings.count > 0
+      return (ratings.sum / ratings.count)
+    else
+      "There aren't any ratings for this product yet."
+    end
+  end
 end
