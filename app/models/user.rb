d726efe21856343db0e7ac9c1f2b88a7479f4e68
Brianna Kemp <kemp.bri@gmail.com>
2019-10-29 15:45:07 -0700

    Added revenue and order count models

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,34 @@ class User < ApplicationRecord
   end
   
   def total_revenue
-    orders = Order.where('status = ?', 'paid')
+    order_items = OrderItem.where(product_id: Product.where(user_id: self.id)).where(order_id: Order.where.not(status: ['pending', 'cancelled']))
+    order_items = order_items.map do |item|
+      item.product.price * item.qty
+    end
+    return order_items.sum
+  end
+  
+  def total_revenue_by_status(status)
+    order_items = OrderItem.where(product_id: Product.where(user_id: self.id)).where(order_id: Order.where(status: status))
+    order_items = order_items.map do |item|
+      item.product.price * item.qty
+    end
+    return order_items.sum
+  end
+  
+  def order_count(status = nil)
+    if status == nil
+      order_items = OrderItem.where(product_id: Product.where(user_id: self.id))
+    else
+      order_items = OrderItem.where(product_id: Product.where(user_id: self.id)).where(order_id: Order.where(status: status))
+    end
+    orders = []
+    order_items.each do |item|
+      if orders.include?(item.order_id)
+      else
+        orders << item.order_id
+      end
+    end
+    return orders.count
   end
 end
