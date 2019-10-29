0bb993e827b5d7ef0bb9993e7fa223b049848182
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-29 13:45:40 -0700

    added most controller tests and passed

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -45,8 +45,6 @@ describe OrdersController do
         get edit_order_path(order.id)
         
         must_respond_with :success
-        
-        
       end
       
       it "will redirect if given an invalid order" do
@@ -58,11 +56,37 @@ describe OrdersController do
         must_respond_with :redirect
         
       end
-      
+    end
+    
+    describe "checkout" do
+      it "can get the checkout path" do
+        perform_login
+        
+        get checkout_path
+        must_respond_with :success
+      end
       
     end
     
-    
+    describe "update" do
+      it "can update an existin order with correct data" do
+        perform_login
+        order = orders(:order1)
+        
+        updates = {
+          order: {
+            status: "paid"
+          }
+        }
+        
+        expect{ patch order_path(order.id), params: updates }.wont_change "Order.count"
+      end
+
+      it "won't update an existing order with bad data" do
+
+      end
+      
+    end
   end
   
   describe "guest user" do
@@ -101,9 +125,45 @@ describe OrdersController do
         
         
       end
+    end
+    
+    describe "checkout" do
+      it "can get the checkout path" do
+        perform_login
+        
+        get checkout_path
+        must_respond_with :success
+      end
       
-      
-      
+    end
+    
+    describe "update" do
+      it "can update an existing order with correct data" do
+        perform_login
+        
+        order = orders(:order1)
+        
+        updates = {
+          order: {
+            status: "paid",
+            email: "kristy@ada.com",
+            ccnum: 1234345656678788
+          }
+        }
+        
+        expect{ patch order_path(order.id), params: updates }.wont_change "Order.count"
+        
+        
+        updated_order = Order.find_by(id: order.id)
+        
+        expect(order.id).must_equal updated_order.id
+        expect(order.ccnum).wont_equal updated_order.ccnum
+        
+      end
+
+      it "won't update an existing order with bad data" do
+
+      end
     end
   end
 end
