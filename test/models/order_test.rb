cbc675b0689e819e07160d3de3b3ef66e5e42571
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-29 16:32:39 -0700

    created order validation tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,160 @@
 require "test_helper"
 
 describe Order do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "should be a valid order" do
+      is_valid = orders(:order1).valid?
+
+      assert(is_valid)
+    end
+
+    it "is invalid if a name is not present and status is not pending" do
+      order = orders(:order1)
+      order.name = ""
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if an email is not present" do
+      order = orders(:order1)
+      order.email = ""
+      order.status = "paid"
+
+
+      is_valid = order.valid?
+      
+      refute(is_valid)
+    end
+
+    it "is invalid if the email address is not inputted correctly" do
+      order = orders(:order1)
+      order.email = "notvalid@1234.cmmmm"
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+
+      order.email = "2fadfdsf.com"
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if an address is not present" do
+      order = orders(:order1)
+      order.address = ""
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if an zip code is not present" do
+      order = orders(:order1)
+      order.zip = ""
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if a zip code is not a 5 digit number" do
+      order = orders(:order1)
+      order.zip = "abcdeqfg"
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+
+      order.zip = 1234567900
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if the credit card number is not present" do
+      order = orders(:order1)
+      order.ccnum = ""
+      order.status = "paid"
+
+      is_valid = order.valid?
+
+      refute(is_valid)
+    end
+
+    it "is invalid if the credit card number is not a 16 digit number" do
+      order = orders(:order1)
+      order.ccnum = "avfjksdlfjas;f"
+      order.status = "paid"
+
+      is_valid = order.valid?
+      refute(is_valid)
+
+      order.ccnum = 12345
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+
+    it "is invalid if the expdate is not present" do
+      order = orders(:order1)
+      order.expdate = ""
+      order.status = "paid"
+
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+
+    it "is invalid if the expdate is not a 5 character string with four numbers seperated by a backslash in the middle" do
+      order = orders(:order1)
+      order.expdate = "as/dfd"
+      order.status = "paid"
+
+      is_valid = order.valid?
+      refute(is_valid)
+
+      order.expdate = "132/123"
+
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+
+    it "is invalid if the CCV is not present" do
+      order = orders(:order1)
+      order.status = "paid"
+      order.ccv = ""
+
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+
+    it "is invalid if the CCV is not a three digit number" do
+      order = orders(:order1)
+      order.status = "paid"
+      order.ccv = 123456
+
+      is_valid = order.valid?
+      refute(is_valid)
+
+      order.ccv = "abc"
+
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+
+    it "is invalid if the status is not present" do
+      order = orders(:order1)
+      order.status = "paid"
+      order.status = ""
+
+      is_valid = order.valid?
+      refute(is_valid)
+    end
+  end
 end
