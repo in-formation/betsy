5b3f6a0143a9067885dbf8dfe83527d79d14d57c
Brianna Kemp <kemp.bri@gmail.com>
2019-10-29 14:08:31 -0700

    merging master into dashboard branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :products
-
+  
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-
+  
   def self.build_from_github(auth_hash)
     user = User.new
     user.uid = auth_hash[:uid]
@@ -11,5 +11,9 @@ class User < ApplicationRecord
     user.name = auth_hash["info"]["nickname"]
     user.email = auth_hash["info"]["email"]
     return user
+  end
+  
+  def total_revenue
+    orders = Order.where('status = ?', 'paid')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_151420) do
+ActiveRecord::Schema.define(version: 2019_10_29_172135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_151420) do
     t.string "email"
     t.string "address"
     t.string "name"
-    t.integer "ccnum"
+    t.bigint "ccnum"
     t.string "expdate"
     t.integer "ccv"
     t.string "zip"
