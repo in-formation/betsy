635cd411a9b137708dc80d2ee322ccff793861e6
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-31 09:35:35 -0700

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_172135) do
+ActiveRecord::Schema.define(version: 2019_10_30_195141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_172135) do
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "order_place"
   end
 
   create_table "products", force: :cascade do |t|
