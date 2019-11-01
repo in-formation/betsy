5f1323d83b62345855546a405cb0cf06e97c429d
Farah Davoodi <farahdavoodi@gmail.com>
2019-11-01 11:18:42 -0700

    fixed seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,7 @@ CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
   product.description = row['description']
   product.status = row['status']
   product.photo_url = row['photo_url']
-  product.user_id = User.find(rand(1..17)).id
+  product.user_id = User.find(rand(1..6)).id
   product.categories << Category.find(rand(1..5))
   successful = product.save
   if !successful
